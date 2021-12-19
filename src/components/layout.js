6216333f6c9764eb558c9e8b62cb5124a72ca3c5
Noah Manger <noah.manger@gmail.com>
2021-12-19 12:51:24 -0800

    Lodging page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,5 +2,5 @@ import React from "react";
 import "../styles/main.scss";
 
 export default function Layout({ title, children }) {
-  return <html lang="en">{children}</html>;
+  return <div>{children}</div>;
 }
--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/layout";
+import monogram from "../images/monogram.png";
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -9,15 +10,13 @@ export default function Template({
   const { frontmatter, html } = markdownRemark;
   return (
     <Layout title={frontmatter.title}>
-      <main>
-        <div className="content">
-          <Link to="/" class="home-link">
-            ← Home
-          </Link>
-          <h1>{frontmatter.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: html }}></div>
-        </div>
-      </main>
+      <div className="content">
+        <Link to="/">
+          <img class="logo" src={monogram} alt="J & N" />
+        </Link>
+        <h1>{frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+      </div>
     </Layout>
   );
 }
