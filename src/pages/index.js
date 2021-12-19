c8684024ca3d5e5b6e5c15f4e18126bb584e30a1
Noah Manger <noah.manger@gmail.com>
2021-12-19 12:33:22 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,11 @@ const IndexPage = () => {
     <Layout title="Home">
       <main>
         <div class="home">
-          <img src={std} alt="Jacqi and Noah in Palm Springs" class="hero" />
+          <img
+            src={std}
+            alt="Jacqi and Noah Make it Official - June 4, 2022"
+            class="hero"
+          />
         </div>
         <Link className="cta" to="/lodging">
           → → → Book your lodging today ← ← ←
