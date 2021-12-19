c446210c2387bb4fdf4201760f15a538deb34672
Noah Manger <noah.manger@gmail.com>
2021-12-19 12:33:02 -0800

    New home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,15 @@
 import { Link } from "gatsby";
 import * as React from "react";
 import Layout from "../components/layout";
-import engaged from "../images/engaged.jpg";
+import std from "../images/save-the-date.png";
 
 // markup
 const IndexPage = () => {
   return (
     <Layout title="Home">
       <main>
-        <div class="banner">
-          <a href="https://www.youtube.com/watch?v=pjB1QK43zxM">
-            Take me I'm yours
-          </a>
-        </div>
         <div class="home">
-          <img
-            src={engaged}
-            alt="Jacqi and Noah in Palm Springs"
-            class="hero"
-          />
-          <div className="intro">
-            <div>
-              <h1>Jacqi Ko & Noah Manger</h1>
-              <p>June 4, 2022</p>
-              <p>McMinnville, Oregon</p>
-            </div>
-          </div>
+          <img src={std} alt="Jacqi and Noah in Palm Springs" class="hero" />
         </div>
         <Link className="cta" to="/lodging">
           → → → Book your lodging today ← ← ←
